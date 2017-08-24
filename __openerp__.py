b44f1e3230a9a4746ec8108f9ece6263cb67ea84
davidsetiyadi <davidsetiadi11@gmail.com>
2017-08-24 21:13:29 +0700

    first update

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    "name"          : "Attendance Website",
+    "category"      : "Custom Development",
+    "author"        : "David",
+    "website"       : "https://github.com/davidsetiyadi",
+    "version"       : "1.0",
+    "description"   : """Attendance Website Template """,
+    "depends"       : ['website'],
+    "data"          : [
+        'views/templates.xml',
+        # 'views/frontend.xml',
+        # 'views/website_search.xml',
+    ],
+    'demo'          : [],
+    'qweb'          : [],
+    'installable'   : True,
+    'application'   : True,
+}
